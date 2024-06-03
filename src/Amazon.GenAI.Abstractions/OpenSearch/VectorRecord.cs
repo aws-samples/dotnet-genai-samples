ca3b066724ead458bb6ed5d73200ee106296c577
Ty Augustine <taugustj@amazon.com>
2024-06-03 09:46:50 -0400

    removed old projects

--- src/Amazon.GenAI.Abstractions/OpenSearch/VectorRecord.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Amazon.GenAI.Abstractions.OpenSearch;
-
-public class VectorRecord
-{
-    public required string Id { get; set; }
-    public string? Text { get; set; }
-    public required float[] Vector { get; set; }
-}
--- src/Amazon.GenAI.Abstractions/Pdf/ICreatableFromStream.cs
+++
@@ -1,8 +0,0 @@
-﻿#if NET7_0_OR_GREATER
-namespace Amazon.GenAI.Abstractions.Pdf;
-
-public interface ICreatableFromStream<out TSelf> where TSelf : ICreatableFromStream<TSelf>
-{
-    public static abstract TSelf CreateFromStream(Stream stream);
-}
-#endif

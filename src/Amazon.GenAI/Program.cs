0fb4ccbc2241e3d07397563b96cad2b3d9f00a8c
abhtia <abhtia@WSAMZN-JFD705NP>
2024-09-11 09:24:30 -0400

    Fixing DI changes and making the app work with the correct region

--- src/Amazon.GenAI/Program.cs
+++ src/Amazon.GenAI/Program.cs
@@ -1,5 +1,7 @@
 using Amazon;
 using Amazon.Bedrock;
+using Amazon.BedrockAgent;
+using Amazon.BedrockAgentRuntime;
 using Amazon.BedrockRuntime;
 using Amazon.GenAI;
 using Amazon.Runtime;
@@ -9,88 +11,111 @@ using Markdig;
 using Markdown.ColorCode;
 using MudBlazor.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-
-builder.Services.AddMudServices();
-
-var pipeline = new MarkdownPipelineBuilder()
-	.UseAdvancedExtensions()
-	.UseColorCode()
-	.Build();
-
-builder.Services.AddSingleton(pipeline);
-
-var regionEndpoint = RegionEndpoint.GetBySystemName(Constants.Region);
-
-AWSCredentials awsCredentials;
-var chain = new CredentialProfileStoreChain();
-
-if (chain.TryGetAWSCredentials("Bedrock", out awsCredentials))
+internal class Program
 {
-	var client = new AmazonBedrockClient(awsCredentials);
+	private static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
 
-	builder.Services.AddSingleton<AmazonBedrockRuntimeClient>(
-	new AmazonBedrockRuntimeClient(awsCredentials, new AmazonBedrockRuntimeConfig()
-	{
-		RegionEndpoint = regionEndpoint
-	}));
+		builder.Services.AddRazorPages();
+		builder.Services.AddServerSideBlazor();
 
-	builder.Services.AddSingleton<AmazonBedrockClient>(
-	new AmazonBedrockClient(awsCredentials, new AmazonBedrockConfig()
-	{
-		RegionEndpoint = regionEndpoint
-	}));
+		builder.Services.AddMudServices();
 
-	builder.Services.AddSingleton<AmazonS3Client>(
-	new AmazonS3Client(new AmazonS3Config
-	{
-		RegionEndpoint = regionEndpoint
-	}));
-}
-else
-{
-	builder.Services.AddSingleton<AmazonBedrockRuntimeClient>(
-	new AmazonBedrockRuntimeClient(new AmazonBedrockRuntimeConfig()
-	{
-		RegionEndpoint = regionEndpoint
-	}));
-	builder.Services.AddSingleton<AmazonBedrockClient>(
-		new AmazonBedrockClient(new AmazonBedrockConfig()
+		var pipeline = new MarkdownPipelineBuilder()
+			.UseAdvancedExtensions()
+			.UseColorCode()
+			.Build();
+
+		builder.Services.AddSingleton(pipeline);
+
+
+
+		AWSCredentials awsCredentials;
+		var chain = new CredentialProfileStoreChain();
+        var regionEndpoint = RegionEndpoint.GetBySystemName(Constants.Region);
+
+        if (chain.TryGetAWSCredentials("KBUser", out awsCredentials))
 		{
-			RegionEndpoint = regionEndpoint
-		}));
+			var client = new AmazonBedrockClient(awsCredentials);
+			//var regionEndpoint = awsCredentials.GetCredentials().Token;
+			builder.Services.AddSingleton(
+			new AmazonBedrockRuntimeClient(awsCredentials, new AmazonBedrockRuntimeConfig()
+			{
+				RegionEndpoint = regionEndpoint
+			}));
 
-	builder.Services.AddSingleton<AmazonS3Client>(
-		new AmazonS3Client(new AmazonS3Config
+			builder.Services.AddSingleton(
+			new AmazonBedrockClient(awsCredentials, new AmazonBedrockConfig()
+			{
+				//RegionEndpoint = regionEndpoint
+			}));
+
+			builder.Services.AddSingleton(
+			new AmazonS3Client(new AmazonS3Config
+			{
+				//RegionEndpoint = regionEndpoint
+			}));
+
+			builder.Services.AddSingleton(
+			new AmazonBedrockAgentClient(awsCredentials, new AmazonBedrockAgentConfig()
+			{
+				RegionEndpoint = regionEndpoint
+			}));
+
+			builder.Services.AddSingleton(
+			new AmazonBedrockAgentRuntimeClient(awsCredentials, new AmazonBedrockAgentRuntimeConfig
+			{
+				RegionEndpoint = regionEndpoint
+			}));
+		}
+		else
 		{
-			RegionEndpoint = regionEndpoint
-		}));
+			builder.Services.AddSingleton(
+			new AmazonBedrockRuntimeClient(new AmazonBedrockRuntimeConfig()
+			{
+			//	RegionEndpoint = regionEndpoint
+			}));
+			builder.Services.AddSingleton(
+				new AmazonBedrockClient(new AmazonBedrockConfig()
+				{
+				//	RegionEndpoint = regionEndpoint
+				}));
+
+			builder.Services.AddSingleton(
+				new AmazonS3Client(new AmazonS3Config
+				{
+					//RegionEndpoint = regionEndpoint
+				}));
+		}
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (!app.Environment.IsDevelopment())
+		{
+			app.UseExceptionHandler("/Error");
+			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			app.UseHsts();
+		}
+
+		app.UseHttpsRedirection();
+
+		app.UseStaticFiles();
+
+		app.UseRouting();
+
+		app.MapBlazorHub();
+		app.MapFallbackToPage("/_Host");
+
+		app.Run();
+	}
 }
 
 #if DEBUG
-builder.Logging.AddDebug();
+
 #endif
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-	app.UseExceptionHandler("/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();

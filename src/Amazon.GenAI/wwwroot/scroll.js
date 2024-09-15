f5890d4bdbd92429314751a4df6a02a5c2a1e90e
Ty Augustine <taugustj@amazon.com>
2024-09-15 17:44:07 -0400

    more UI updates

--- src/Amazon.GenAI/wwwroot/scroll.js
+++ src/Amazon.GenAI/wwwroot/scroll.js
@@ -1,3 +1,9 @@
-﻿ function scrollToElement(id) {
-     document.getElementById(id).scrollIntoView()
+﻿function scrollToElement(id) {
+    const container = document.getElementById(id);
+    if (container) {
+        console.log(container.lastElementChild);
+        container.lastElementChild.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    } else {
+        console.error('Container element not found');
+    }
 }

2af7d9c8d1c750d411b4e1752fe33258b57268a8
abhtia <abhtia@WSAMZN-JFD705NP>
2024-10-11 10:53:11 -0400

    Validating git commit changes

--- src/Amazon.GenAI/Constants.cs
+++ src/Amazon.GenAI/Constants.cs
@@ -3,7 +3,7 @@
 public static class Constants
 {
     public static string? Region { get; set; } = "us-west-2";
-    public static string KnowledgeBaseName { get; set; } = "octank-hotels";
+    public static string KnowledgeBaseName { get; set; } = "knowledge-base";
     public static string TextModelId { get; set; } = "anthropic.claude-3-sonnet-20240229-v1:0";
     public static string ImageModelId { get; set; } = "stability.sd3-large-v1:0";
     public static string BucketName { get; set; } = "dotnet-rag-datasource";

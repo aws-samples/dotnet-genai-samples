7e30af1e5a19fcdaa42f8b4137a119030fc57f19
Jagadeesh Chitikesi <chitikj@amazon.com>
2024-11-05 10:21:23 -0500

    Workshop changes

--- src/Amazon.GenAI/Constants.cs
+++ src/Amazon.GenAI/Constants.cs
@@ -8,16 +8,4 @@ public static class Constants
     public static string ImageModelId { get; set; } = "stability.sd3-large-v1:0";
     public static string BucketName { get; set; } = "dotnet-rag-datasource";
     public static string MetadataExtension { get; set; } = ".json";
-    public static string[] BuiltinPrompts { get; set; } =
-    [
-        "What are the check-in and check-out times?",
-        "Is there shuttle service to the airport?",
-        "Does the room rate include breakfast?",
-        "Do you have a fitness center or swimming pool?",
-        "Can I bring my pet?",
-        "What are the dining options?",
-        "What type of views does the room offer?",
-        "Is there a concierge service?"
-    ];
-   public static string AdditionalInstructions { get; set; } = ".Where possible, include the Octank location(s) in your response.";
 }

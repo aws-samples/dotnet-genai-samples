22c6f70042c8bcfeca313dad782833d89eb4997b
Ty Augustine <taugustj@amazon.com>
2025-05-06 11:26:20 -0400

    updated modelId

--- src/Amazon.GenAI/Constants.cs
+++ src/Amazon.GenAI/Constants.cs
@@ -4,7 +4,7 @@ public static class Constants
 {
     public static string? Region { get; set; } = "us-west-2";
     public static string KnowledgeBaseName { get; set; } = "octank-hotels-faq-kb";
-    public static string TextModelId { get; set; } = "anthropic.claude-3-sonnet-20240229-v1:0";
+    public static string TextModelId { get; set; } = "anthropic.claude-3-5-sonnet-20241022-v2:0";
     public static string ImageModelId { get; set; } = "stability.sd3-large-v1:0";
     public static string BucketName { get; set; } = "dotnet-rag-datasource";
     public static string MetadataExtension { get; set; } = ".json";

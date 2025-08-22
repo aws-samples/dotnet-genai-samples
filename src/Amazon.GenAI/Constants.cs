9cadc74333ead753a8a516fb440b702fae28f3ef
Jagadeesh Chitikesi <chitikj@amazon.com>
2025-08-22 16:36:38 -0400

    BedrockAgent.razor page updates

--- src/Amazon.GenAI/Constants.cs
+++ src/Amazon.GenAI/Constants.cs
@@ -1,11 +1,15 @@
-﻿namespace Amazon.GenAI;
+﻿using Amazon.BedrockAgent;
+
+namespace Amazon.GenAI;
 
 public static class Constants
 {
     public static string? Region { get; set; } = "us-west-2";
     public static string KnowledgeBaseName { get; set; } = "octank-hotels-faq-kb";
-    public static string TextModelId { get; set; } = "anthropic.claude-3-5-sonnet-20241022-v2:0";
+    public static string TextModelId { get; set; } = "us.anthropic.claude-3-5-sonnet-20241022-v2:0";
     public static string ImageModelId { get; set; } = "stability.sd3-large-v1:0";
     public static string BucketName { get; set; } = "dotnet-rag-datasource";
     public static string MetadataExtension { get; set; } = ".json";
-} 
+    public static string AgentId { get; set; } = string.Empty;
+    public static string AgentAliasId { get; set; } = string.Empty;
+}
